93adaccc98e159c43498ed97c89dbfc311ae3f5e
clangdon1023 <139593626+clangdon1023@users.noreply.github.com>
2023-10-24 08:04:40 -0400

    Update bonus.js

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,24 +1,24 @@
-// Fetch the JSON data and console log it
+
 d3.json(url).then(function(data) {
   console.log(data);
 });
 
-// Initialize the dashboard at start up 
+
 function init() {
 
-    // Use D3 to select the dropdown menu
+   
     let dropdownMenu = d3.select("#selDataset");
 
-    // Use D3 to get sample names and populate the drop-down selector
+   
     d3.json(url).then((data) => {
         
-        // Set a variable for the sample names
+        // Sample names variable
         let names = data.names;
 
-        // Add  samples to dropdown menu
+       
         names.forEach((id) => {
 
-            // Log the value of id for each iteration of the loop
+           
             console.log(id);
 
             dropdownMenu.append("option")
@@ -26,39 +26,39 @@ function init() {
             .property("value",id);
         });
 
-        // Set the first sample from the list
+       
         let sample_one = names[0];
 
-        // Log the value of sample_one
+        
         console.log(sample_one);
 
-        // Build the initial plots
+       
         buildGaugeChart(sample_one);
     });
 };
 
-// Function that builds the gauge chart
+// guage chart function
 function buildGaugeChart(sample) {
 
-    // Use D3 to retrieve all of the data
+   
     d3.json(url).then((data) => {
 
-        // Retrieve all metadata
+       
         let metadata = data.metadata;
 
-        // Filter based on the value of the sample
+     
         let value = metadata.filter(result => result.id == sample);
 
-        // Log the array of metadata objects after the have been filtered
+    
         console.log(value)
 
-        // Get the first index from the array
+        
         let valueData = value[0];
 
-        // Use Object.entries to get the key/value pairs and put into the demographics box on the page
+       
         let washFrequency = Object.values(valueData)[6];
         
-        // Set up the trace for the gauge chart
+    
         let trace2 = {
             value: washFrequency,
             domain: {x: [0,1], y: [0,1]},
@@ -86,17 +86,17 @@ function buildGaugeChart(sample) {
             } 
         };
 
-        // Set up the Layout
+        // layout
         let layout = {
             width: 400, 
             height: 400,
             margin: {t: 0, b:0}
         };
 
-        // Call Plotly to plot the gauge chart
+        
         Plotly.newPlot("gauge", [trace2], layout)
     });
 };
 
-// Call the initialize function
+
 init();
